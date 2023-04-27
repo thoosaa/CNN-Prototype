5bc923d28b7125ecfacf764f25e8e6685a7ba0dd
Tatyana Sugako <thoosa@mail.ru>
2023-04-28 01:05:51 +0300

    Model_6

--- model_7.py
+++ model_7.py
@@ -1,30 +1,31 @@
 from tensorflow import keras
 from keras import layers
+from keras import activations
 
 input_shape = (750, 750, 3)
 
 inputs = keras.Input(shape=input_shape)
 
-x = layers.Conv2D(8, kernel_size=(7, 7), activation="leaky relu", padding="valid")(inputs)
+x = layers.Conv2D(8, kernel_size=(7, 7), padding="valid", activation=activations.leaky_relu)(inputs)
 x = layers.MaxPooling2D(pool_size=(2, 2), strides=1)(x)
-x = layers.Conv2D(16, kernel_size=(5, 5), activation="leaky relu", padding="valid")(x)
+x = layers.Conv2D(16, kernel_size=(5, 5), activation=activations.leaky_relu)(x)
 x = layers.MaxPooling2D(pool_size=(2, 2), strides=1)(x)
-x = layers.Conv2D(32, kernel_size=(3, 3), activation="softmax", padding="valid")(x)
+x = layers.Conv2D(32, kernel_size=(3, 3), activation=activations.leaky_relu)(x)
 x = layers.MaxPooling2D(pool_size=(3, 3), strides=1)(x)
-x = layers.Conv2D(32, kernel_size=(3, 3), activation="leaky relu", padding="valid")(x)
+x = layers.Conv2D(32, kernel_size=(3, 3), activation=activations.leaky_relu)(x)
 x = layers.MaxPooling2D(pool_size=(3, 3), strides=1)(x)
-x = layers.Conv2D(16, kernel_size=(5, 5), activation="leaky relu", padding="valid")(x)
+x = layers.Conv2D(16, kernel_size=(5, 5), activation=activations.leaky_relu)(x)
 x = layers.MaxPooling2D(pool_size=(3, 3), strides=1)(x)
 x = layers.Flatten()(x)
 
-x = layers.Dense(1024, activation="leaky relu")(x)
-x = layers.Dense(1024, activation="leaky relu")(x)
+x = layers.Dense(1024, activation=activations.leaky_relu)(x)
+x = layers.Dense(1024, activation=activations.leaky_relu)(x)
 x = layers.Dropout(0.2)(x)
-x = layers.Dense(1024, activation="leaky relu")(x)
+x = layers.Dense(1024, activation=activations.leaky_relu)(x)
 x = layers.Dropout(0.2)(x)
-x = layers.Dense(512, activation="leaky relu")(x)
+x = layers.Dense(512, activation=activations.leaky_relu)(x)
 x = layers.Dropout(0.2)(x)
-x = layers.Dense(512, activation="leaky relu")(x)
+x = layers.Dense(512, activation=activations.leaky_relu)(x)
 
 click_type = layers.Dense(10, activation="softmax", name="click_type")(x)
 keys = layers.Dense(5, activation="sigmoid", name="keys")(x)
@@ -32,6 +33,10 @@ x_coord = layers.Dense(1, name="x_coord")(x)
 y_coord = layers.Dense(1, name="y_coord")(x)
 
 model = keras.Model(inputs=inputs, outputs=[click_type, keys, x_coord, y_coord])
+
+model = model.to_json()
+with open("model_7.json", "w") as json_file:
+    json_file.write(model)
 
 model.summary()
 
@@ -52,12 +57,6 @@ model.compile(
     },
 )
 '''
-
-model = model.to_json()
-with open("model_7.json", "w") as json_file:
-    json_file.write(model)
-
-
 
 
 
