c09c1bb7504bef5adb502a2433e1da760def1b2b
Tatyana Sugako <thoosa@mail.ru>
2023-05-06 11:45:11 +0300

    Model_6

--- model_6.py
+++ model_6.py
@@ -18,30 +18,23 @@ x = layers.Conv2D(16, kernel_size=(3, 3), activation=activations.leaky_relu)(x)
 x = layers.MaxPooling2D(pool_size=(2, 2), strides=1)(x)
 x = layers.Flatten()(x)
 
-x = layers.Dense(1024, activation=activations.leaky_relu)(x)
+x = layers.Dense(128, activation=activations.leaky_relu)(x)
 x = layers.Dropout(0.7)(x)
-x = layers.Dense(1024, activation=activations.leaky_relu)(x)
-x = layers.Dense(1024, activation=activations.leaky_relu)(x)
-x = layers.Dense(512, activation="tanh")(x)
-x = layers.Dense(512, activation="tanh")(x)
-x = layers.Dense(512, activation="relu")(x)
-x = layers.Dense(256, activation="relu")(x)
-x = layers.Dense(512, activation=activations.leaky_relu)(x)
+x = layers.Dense(128, activation=activations.leaky_relu)(x)
+x = layers.Dense(128, activation=activations.leaky_relu)(x)
+x = layers.Dense(128, activation=activations.tanh)(x)
+x = layers.Dense(128, activation=activations.tanh)(x)
+x = layers.Dense(128, activation=activations.relu)(x)
+x = layers.Dense(128, activation=activations.relu)(x)
+x = layers.Dense(128, activation=activations.leaky_relu)(x)
 
-click_type = layers.Dense(10, activation="softmax", name="click_type")(x)
-keys = layers.Dense(5, activation="sigmoid", name="keys")(x)
+click_type = layers.Dense(10, activation=activations.softmax, name="click_type")(x)
+keys = layers.Dense(5, activation=activations.sigmoid, name="keys")(x)
 x_coord = layers.Dense(1, name="x_coord")(x)
 y_coord = layers.Dense(1, name="y_coord")(x)
 
 model = keras.Model(inputs=inputs, outputs=[click_type, keys, x_coord, y_coord])
 
-model = model.to_json()
-with open("model_6.json", "w") as json_file:
-    json_file.write(model)
-
-model.summary()
-
-'''
 model.compile(
     optimizer=keras.optimizers.Adam(),
     loss={
@@ -57,7 +50,11 @@ model.compile(
         "number2": "mae",
     },
 )
-'''
+model.summary()
+
+model = model.to_json()
+with open("model_6.json", "w") as json_file:
+    json_file.write(model)
 
 
 
